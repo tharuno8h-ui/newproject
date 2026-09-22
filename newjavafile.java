851e50de8efd51d835d4c4fde02d4fd4f8c7fb20
tharun <tharuno8h@gmail.com>
2026-09-22 11:20:48 +0530

    javafile

---
+++ newjavafile.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.print(a*100*b);// your code goes here
+		
+
+	}
+}
